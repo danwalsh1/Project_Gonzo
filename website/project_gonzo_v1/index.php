a8797560082929275e8394161e6504d037f6a8af
Daniel Walsh <33032585+danwalsh1@users.noreply.github.com>
2018-01-25 14:27:09 +0000

    Created index.php
    This file is the default file that opens if the root folder is searched.

---
+++ website/project_gonzo_v1/index.php
@@ -0,0 +1,21 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+    <div id="Page">
+      <div id="Header">
+      </div>
+      
+      <div id="Content">
+      </div>
+      
+      <div id="Footer">
+      </div>
+    </div>
+  </body>
+</html>
