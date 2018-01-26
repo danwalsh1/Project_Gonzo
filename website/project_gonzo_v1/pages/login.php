1544bbb9257d692df53f0e0267d57bbc470d660b
GarethHolloway <33927441+GarethHolloway@users.noreply.github.com>
2018-01-26 12:24:36 +0000

    UPDATED login.php
    Configured the login form and enabled credential checking by querying the users table in the database and then providing access and forwarding to the site if the credentials are valid.

--- website/project_gonzo_v1/pages/login.php
+++ website/project_gonzo_v1/pages/login.php
@@ -1,15 +1,39 @@
 <?php
-  session_start();
+  session_start(); #Starts a session.
   
   include("../functions/generalFunctions.php");
   include("../functions/sqlFunctions.php");
   
   check_db_exists();
+  
+  if (isset($_POST['logindone'])){# checks credentials are valid and then passes through to homepage.
+	$result = UserLogin($_POST['Username'],$_POST['Password']); #Stores input username and password.
+	if($result){ #if result is true, continues through to homepage otherwise remains on login page.
+		$_SESSION["Username"] = $_POST['Username']; #Post assigned to session to carry username to next page.
+		header ("Location: home.php");
+		die();
+	}
+  }
+  
+function UserLogin($username,$password){ #Passes in username and password from html form.
+$connect = connect_db(); #connect to db
+$sql = 'SELECT * FROM users WHERE username="' . $username . '" AND password="' . $password . '"'; # checking validity of credentials.
+$result = mysqli_query($connect,$sql); #Executed query stored in $result.
+if(mysqli_num_rows($result)== 1){ #Conditions checked, passes over to $result in the start if statement.
+	return true;
+	}
+else{
+	return false;
+	}
+}
+	
 ?>
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Login | Project Gonzo</title>
+  <link href="../CSS/mainMenu.css" rel  = "stylesheet" type="text/CSS" />
+  <link href="../CSS/mainLayout.css" rel  = "stylesheet" type="text/CSS" />
+    <title>Login | Project Gonzo</title> <!--Project title -->
   </head>
   <body>
     <div id="Page">
@@ -22,10 +46,24 @@
 			</nav>
 		</div>
       </div>
-      
-      <div id="Content">
+	  
+	 
+      <div style ="text-align: center;"> <!--Form Configuration and positioning -->
+	  <form method ="POST" action="login.php">
+       <div id="UsernameText">
+		<label> Username: </label>
+		<input type ="text" name="Username">
+		</div>
+	   <div id="PasswordText" style="padding-top: 10px;">
+		<label> Password: </label>
+		<input type="password" name ="Password">
+		<div style="padding-top: 10px;padding-left: 50px;">
+		<input type="submit" value="Login" name ="logindone">
+		</div>
+	   </div>
+	  </form>
       </div>
-      
+	  
       <div id="Footer">
       </div>
     </div>
