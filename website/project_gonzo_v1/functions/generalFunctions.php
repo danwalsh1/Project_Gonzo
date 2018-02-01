f3134e71f3cc3cdf27d1d0978767f97b9f7e75ee
Daniel Walsh <33032585+danwalsh1@users.noreply.github.com>
2018-02-01 11:48:19 +0000

    UPDATED generalFunctions.php
    Added elements to the non-admins users menu.

--- website/project_gonzo_v1/functions/generalFunctions.php
+++ website/project_gonzo_v1/functions/generalFunctions.php
@@ -1,10 +1,10 @@
 <?php
 	function displayMainMenu(){
-		if(isset($_SESSION['useradmin'])){
-			if($_SESSION['useradmin'] == true){
+		if(isset($_SESSION['admin'])){
+			if($_SESSION['admin'] == true){
 				echo '<li><a href="home.php">Home</a></li><li><a href="logout.php">Logout</a></li>';
 			}else{
-				echo '<li><a href="home.php">Home</a></li><li><a href="logout.php">Logout</a></li>';
+				echo '<li><a href="home.php">Home</a></li><li><a href="UserProfile.php">My Profile</a></li><li><a href="logout.php">Logout</a></li>';
 			}
 		}else{
 			echo '<li><a href="login.php">Login</a></li>';
