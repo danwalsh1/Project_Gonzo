39a948167b01ba442a9b17ce3f2353e7074f52fc
Daniel Walsh <33032585+danwalsh1@users.noreply.github.com>
2018-02-01 12:23:56 +0000

    UPDATED generalFunctions.php
    Updated the main menu

--- website/project_gonzo_v1/functions/generalFunctions.php
+++ website/project_gonzo_v1/functions/generalFunctions.php
@@ -2,9 +2,9 @@
 	function displayMainMenu(){
 		if(isset($_SESSION['admin'])){
 			if($_SESSION['admin'] == true){
-				echo '<li><a href="home.php">Home</a></li><li><a href="logout.php">Logout</a></li>';
+				echo '<li><a href="home.php">Home</a></li><li><a href="adminCharts.php">Sensor Data</a></li><li><a href="">Administration</a></li><li><a href="logout.php">Logout</a></li>';
 			}else{
-				echo '<li><a href="home.php">Home</a></li><li><a href="UserProfile.php">My Profile</a></li><li><a href="logout.php">Logout</a></li>';
+				echo '<li><a href="home.php">Home</a></li><li><a href="userCharts.php">My Device</a></li><li><a href="UserProfile.php">My Profile</a></li><li><a href="logout.php">Logout</a></li>';
 			}
 		}else{
 			echo '<li><a href="login.php">Login</a></li>';
