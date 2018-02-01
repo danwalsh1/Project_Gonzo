9ad8ff77fabe29dea81dbc7dc28cbca0316e956a
Jacob Stenson <jacob@nosnets.co.uk>
2018-02-01 18:25:30 +0000

    UPDATED AdminPage.php
    Added all of the text fields for "Username", "Password" ETC.
    This is not finished as things are not aligned properly yet but its a basis of my idea.

--- website/project_gonzo_v1/pages/AdminPage.php
+++ website/project_gonzo_v1/pages/AdminPage.php
@@ -1,19 +1,30 @@
 <?php
   session_start();
-  
+
   include("../functions/generalFunctions.php");
-  
+
   if(!isset($_SESSION['Username'])){
 	  header("Location: login.php");
 	  die();
   }
+
 ?>
+<style>
+* {padding: 0; margin: 0;}
+#TextFields
+{
+  width:300px;
+  height:auto;
+  text-align: center;
+}
+</style>
+
 <!DOCTYPE html>
 <html>
   <head>
     <link href="../CSS/mainMenu.css" rel  = "stylesheet" type="text/CSS" />
     <link href="../CSS/mainLayout.css" rel  = "stylesheet" type="text/CSS" />
-    <title>AdminPage | Project Gonzo</title>
+    <title>Admin Page | Project Gonzo</title>
   </head>
   <body>
     <div id="Page">
@@ -26,27 +37,59 @@
 			</nav>
 		</div>
       </div>
-      
+
       <div id="Content">
 	  <h1> Admin Page </h1>
-	  <p> Hello </p>
+    <br>
 	  <form action="/action_page.php">
+      Select Username:
 	    <input list="browsers" name="browser">
 		<datalist id="browsers">				<!-- Below should be changed to fit the needs of our data using PHP. -->
 		  <option value="THIS ISNT DONE YET">
-		  <option value="Internet Explorer">
-		  <option value="Firefox">
-		  <option value="Chrome">
-		  <option value="Opera">
-		  <option value="Safari">
+		  <option value="Daniel Walsh">
+		  <option value="Gareth Holloway">
+		  <option value="Elliot Hawkins">
+		  <option value="Jacob Stenson">
 		</datalist>
+
 		<input type="submit">
 		</form>
+
+    <br>
+<div id="TextFields">
+
+  <form action="action_page.php">              <!-- Below should be changed to fit the needs of our data using PHP. -->
+  Username:
+  <input name="Username" type="text" value="Username here.">
+  <br>
+  Password:
+  <input name="Password" type="text" value="Password here.">
+  <br>
+  <br>
+  Forename:
+  <input name="Forename" type="text" value="Forename here.">
+  <br>
+  Surname:
+  <input name="Surname" type="text" value="Surname here.">
+  <br>
+  Device_ID:
+  <input name="Device_ID" type="text" value="Device_ID here.">
+  <br>
+  Phone Number:
+  <input name="Phone_Num" type="text" value="Phone Number here.">
+  <br>
+  Email:
+  <input name="Email" type="text" value="Email here.">
+  <br><br>
+  <input type="submit">
+</form>
+</div>
+
+
+
 </body>
 </html>
-
       </div>
-      
       <div id="Footer">
       </div>
     </div>
