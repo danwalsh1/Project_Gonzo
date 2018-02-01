ad5dd8ca5a51ff8b120e2800afe0e73ef4e0786f
Jacob Stenson <jacob@nosnets.co.uk>
2018-02-01 18:11:34 +0000

    UPDATED generalFunctions.php
    Added the "AdminPage.php" reference for the Administrator button.

--- website/project_gonzo_v1/functions/generalFunctions.php
+++ website/project_gonzo_v1/functions/generalFunctions.php
@@ -2,7 +2,7 @@
 	function displayMainMenu(){
 		if(isset($_SESSION['admin'])){
 			if($_SESSION['admin'] == true){
-				echo '<li><a href="home.php">Home</a></li><li><a href="adminCharts.php">Sensor Data</a></li><li><a href="">Administration</a></li><li><a href="logout.php">Logout</a></li>';
+				echo '<li><a href="home.php">Home</a></li><li><a href="adminCharts.php">Sensor Data</a></li><li><a href="AdminPage.php">Administration</a></li><li><a href="logout.php">Logout</a></li>';
 			}else{
 				echo '<li><a href="home.php">Home</a></li><li><a href="userCharts.php">My Device</a></li><li><a href="UserProfile.php">My Profile</a></li><li><a href="logout.php">Logout</a></li>';
 			}
