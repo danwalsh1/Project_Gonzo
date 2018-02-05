0f707e685c257df9d5666d18ad64a320c2574c3d
Daniel Walsh <33032585+danwalsh1@users.noreply.github.com>
2018-02-05 15:50:08 +0000

    UPDATED chartFunctions.php
    Added averaging functions

--- website/project_gonzo_v1/functions/chartFunctions.php
+++ website/project_gonzo_v1/functions/chartFunctions.php
@@ -22,4 +22,41 @@
 		</script>
 	";
 	}
+
+	function get_avg_device_data_day($startDate, $endDate, $deviceId){
+		# Start date is inclusive, end date is not inclusive
+		$connect = connect_db();
+		#$sql = "SELECT * FROM device_data WHERE date>='" . $startDate . "' AND date<'" . $endDate . "' AND device_id='" . $deviceId . "'";
+		$sql = "SELECT * FROM device_data WHERE date>='" . $startDate . "' AND date<'" . $endDate . "'";
+		$result = mysqli_query($connect, $sql);
+		if(mysqli_num_rows($result) != 0){
+			while($row = mysqli_fetch_array($result)){
+				$batLevelArray[] = $row['battery_level'];
+			}
+		}else{
+			echo "ERROR";
+		}
+		$count = 0;
+		$avg = 0;
+		while($count < count($batLevelArray)){
+			$avg = $avg + $batLevelArray[$count];
+			$count += 1;
+		}
+		
+		$avg = $avg / count($batLevelArray);
+		return $avg;
+	}
+	
+	function get_avg_device_data_dates($startDate, $numOfDays, $deviceId){
+		$count = 0;
+		while($count < $numOfDays){
+			#echo get_avg_device_data_day($startDate, strtotime("+1 day", strtotime($startDate)), $deviceId);
+			#echo date("Y-m-d", $startDate);
+			$avgLevelArray[] = get_avg_device_data_day($startDate, date("Y-m-d", strtotime("+1 day", strtotime($startDate))), $deviceId);
+			$startDate = date("Y-m-d", strtotime("+1 day", strtotime($startDate)));
+			$count += 1;
+		}
+		
+		return $avgLevelArray;
+	}
 ?>
