7720fbf2cf4927e52e175748826982d319780eb6
Daniel Walsh <33032585+danwalsh1@users.noreply.github.com>
2018-02-05 17:45:18 +0000

    UPDATED UserProfile.php
    Made an error, added wrong page to this file, re-uploaded original code.

--- website/project_gonzo_v1/pages/UserProfile.php
+++ website/project_gonzo_v1/pages/UserProfile.php
@@ -1,23 +1,39 @@
 <?php
-  session_start();
+  session_start(); #Starts a session.
   
-  include("../functions/generalFunctions.php");
+  include("../functions/generalFunctions.php"); #Import additional functionality stored in seperate files.
   include("../functions/sqlFunctions.php");
-  include("../functions/chartFunctions.php");
-  
-  if(!isset($_SESSION['Username'])){
-	  header("Location: login.php");
-	  die();
-  }
-  
-  $userValues = get_users_values($_SESSION['Username']);
+check_db_exists();  
+	
+if (isset($_POST['SubmitProfileForm'])){ #Checks to make sure the $_POST form value exists before processing the function call assigned to the variable.
+	$result = update_user_data();
+	}
+function fetch_user_data($username){
+		$connect = connect_db("NULL"); #NULL as no data retrieval is required yet, simply the connection.
+		$sql = 'SELECT * FROM users WHERE username="'. $_SESSION["Username"] . '"'; #Queries the database for the users details using details stored in session.
+		$result = mysqli_query($connect, $sql); #Run the query.
+		$value = mysqli_fetch_object($result);#Fetch the items identified in the $resulte variable.
+		return $value; #return the fields identified.
+		}
+$value = fetch_user_data($_SESSION['Username']); #Used to automatically the identified user data into the html form.
+		
+function update_user_data(){
+        $connect = connect_db("NULL"); #Connects to DB
+		{
+        $sql = "UPDATE users SET forename='" . $_POST['forename'] . "', surname='" . $_POST['surname'] . "', device_id='" . $_POST['deviceid'] . "', phone_num='" . $_POST['phonenum'] . "', email='" . $_POST['email'] . "', password='" . $_POST['PasswordUpdate'] . "'WHERE username='". $_POST['username'] . "'";
+		} #Updates all table fields as a catch all solution even if the value is the same.		
+        $result = mysqli_query($connect, $sql);
+		#echo $sql; #Testing for the SQL to ensure correct syntax.
+}
 ?>
+
+
 <!DOCTYPE html>
 <html>
   <head>
-    <link href="../CSS/mainMenu.css" rel  = "stylesheet" type="text/CSS" />
-    <link href="../CSS/mainLayout.css" rel  = "stylesheet" type="text/CSS" />
-    <title>Charts | Project Gonzo</title>
+  <link href="../CSS/mainMenu.css" rel  = "stylesheet" type="text/CSS" />
+  <link href="../CSS/mainLayout.css" rel  = "stylesheet" type="text/CSS" />
+    <title>User Profile | Project Gonzo</title> <!--Page title -->
   </head>
   <body>
     <div id="Page">
@@ -30,27 +46,56 @@
 			</nav>
 		</div>
       </div>
-      
-      <div id="Content">
-		<h2>Device ID: <?php echo $userValues->device_id; ?></h2>
-		<?php
-			$result = get_avg_device_data_dates('2018-02-04', 7, 'lapTest');
-
-			$count = 0;
-			$dataString = "[['Date', 'Avg Battery Level'],";
-			while($count < 6){
-				$dataString = $dataString . "['" . $result[1][$count] . "', " . $result[0][$count] . "],";
-				$count += 1;
-			}
-			$dataString = $dataString . "['" . $result[1][$count] . "', " . $result[0][$count] . "]]";
-			
-			#echo $dataString;
-			makeGoogleChart('Data Chart', 500, 300, 'dataChart', $dataString);
-		?>
+	  
+	 
+      <div style ="text-align: center;"> <!--Form Configuration and positioning -->
+	  <form method ="POST" action="UserProfile.php">
+	  
+	   <div id="Username" style="padding-top: 10px;">
+		<label> Username: </label>
+		<input type ="text" name="username" value=<?php echo '"' . $value->username . '"' ?>>   
+	   </div>
+	  <!--As for all the values in the form, the variable $value defined earlier is used to map to each table value to the form field name.-->
+       <div id="FirstNameText" style="padding-top: 10px;">
+		<label> First Name: </label>
+		<input type ="text" name="forename" value=<?php echo '"' . $value->forename . '"' ?>>
+	   </div>
 		
-		<div id="dataChart"></div>
+	   <div id="Surname" style="padding-top: 10px;">
+		<label> Surname: </label>
+		<input type="text" name ="surname" value=<?php echo '"' . $value->surname . '"' ?>>
+	   </div>
+		
+		<div id= "Device_ID" style="padding-top: 10px;">
+		 <label> Device ID: </label>
+		 <input type ="text" name="deviceid" value=<?php echo '"' . $value->device_id . '"' ?>>
+		</div>
+		 
+		
+		<div id= "Phone_Number" style="padding-top: 10px;">
+		 <label> Phone Number: </label>
+		 <input type ="text" name="phonenum" value=<?php echo '"' . $value->phone_num . '"' ?>>
+		</div>
+		
+		<div id= "Email_Address" style="padding-top: 10px;">
+		 <label> Email Address: </label>
+		 <input type ="text" name="email" value=<?php echo '"' . $value->email . '"' ?>>
+		</div>
+		
+		
+		<div id= "Update_Password" style="padding-top: 10px;">
+		 <label> Update Password: </label>
+		 <input type ="password" name="PasswordUpdate" value=<?php echo '"' . $value->password . '"' ?>>
+		</div> 
+		
+
+		<div style="padding-top: 10px;padding-left: 50px;">
+		  <input type="submit" value="Save" name="SubmitProfileForm">
+		</div>
+	   </div>
+	  </form>
       </div>
-      
+	  
       <div id="Footer">
       </div>
     </div>
