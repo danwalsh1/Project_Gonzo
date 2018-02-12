f480f2a5552af71d5e0e2fdaae697b4240616506
Daniel Walsh <33032585+danwalsh1@users.noreply.github.com>
2018-02-12 15:04:10 +0000

    UPDATED chartFunctions.php
    Allows days with missing data to be shown with an average of 0% for that day

--- website/project_gonzo_v1/functions/chartFunctions.php
+++ website/project_gonzo_v1/functions/chartFunctions.php
@@ -27,14 +27,13 @@
 		# Start date is inclusive, end date is not inclusive
 		$connect = connect_db();
 		$sql = "SELECT * FROM device_data WHERE date>='" . $startDate . "' AND date<'" . $endDate . "' AND device_id='" . $deviceId . "'";
-		#$sql = "SELECT * FROM device_data WHERE date>='" . $startDate . "' AND date<'" . $endDate . "'";
 		$result = mysqli_query($connect, $sql);
 		if(mysqli_num_rows($result) != 0){
 			while($row = mysqli_fetch_array($result)){
 				$batLevelArray[] = $row['battery_level'];
 			}
 		}else{
-			echo "ERROR";
+			return 0;
 		}
 		$count = 0;
 		$avg = 0;
@@ -50,8 +49,6 @@
 	function get_avg_device_data_dates($startDate, $numOfDays, $deviceId){
 		$count = 0;
 		while($count < $numOfDays){
-			#echo get_avg_device_data_day($startDate, strtotime("+1 day", strtotime($startDate)), $deviceId);
-			#echo date("Y-m-d", $startDate);
 			$avgLevelArray[] = round(get_avg_device_data_day($startDate, date("Y-m-d", strtotime("+1 day", strtotime($startDate))), $deviceId));
 			$dateArray[] = $startDate;
 			$startDate = date("Y-m-d", strtotime("+1 day", strtotime($startDate)));
