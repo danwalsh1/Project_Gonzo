2c316426f84736c7677fd3d5c3f8415639ec067a
Daniel Walsh <33032585+danwalsh1@users.noreply.github.com>
2018-02-15 14:30:47 +0000

    UPDATED generalFunctions.php
    Added link for admins menu bar to the create users page.

--- website/project_gonzo_v1/functions/generalFunctions.php
+++ website/project_gonzo_v1/functions/generalFunctions.php
@@ -2,7 +2,7 @@
 	function displayMainMenu(){
 		if(isset($_SESSION['admin'])){
 			if($_SESSION['admin'] == true){
-				echo '<li><a href="home.php">Home</a></li><li><a href="adminCharts.php">Sensor Data</a></li><li><a href="AdminPage.php">Administration</a></li><li><a href="logout.php">Logout</a></li><li style="float:right"><a href="home.php"><img src="../img/gonzoLogo.jpg" height="60" width="65" style="align:center" /></a></li>';
+				echo '<li><a href="home.php">Home</a></li><li><a href="adminCharts.php">Sensor Data</a></li><li><a href="AdminPage.php">Administration</a></li><li><a href="createUser.php">Create User</a></li><li><a href="logout.php">Logout</a></li><li style="float:right"><a href="home.php"><img src="../img/gonzoLogo.jpg" height="60" width="65" style="align:center" /></a></li>';
 			}else{
 				echo '<li><a href="home.php">Home</a></li><li><a href="userCharts.php">My Device</a></li><li><a href="UserProfile.php">My Profile</a></li><li><a href="logout.php">Logout</a></li><li style="float:right"><a href="home.php"><img src="../img/gonzoLogo.jpg" height="60" width="65" style="align:center" /></a></li>';
 			}
