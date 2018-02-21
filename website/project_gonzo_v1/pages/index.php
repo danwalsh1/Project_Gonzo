1d356f7c0ed28156cebc52c2b0827844ac077a7b
Daniel Walsh <33032585+danwalsh1@users.noreply.github.com>
2018-02-21 18:49:40 +0000

    UPDATED index.php
    This page immediately redirects the user to the login page, therefore no HTML code is required.

--- website/project_gonzo_v1/pages/index.php
+++ website/project_gonzo_v1/pages/index.php
@@ -1,35 +1,6 @@
 <?php
   session_start();
   
-  include("../functions/generalFunctions.php");
-  
   header("Location: login.php");
   die();
 ?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <link href="../CSS/mainMenu.css" rel  = "stylesheet" type="text/CSS" />
-    <link href="../CSS/mainLayout.css" rel  = "stylesheet" type="text/CSS" />
-    <title>Project Gonzo</title>
-  </head>
-  <body>
-    <div id="Page">
-      <div id="Header">
-		<div id="Menu">
-			<nav>
-				<ul>
-					<?php displayMainMenu(); ?>
-				</ul>
-			</nav>
-		</div>
-      </div>
-      
-      <div id="Content">
-      </div>
-      
-      <div id="Footer">
-      </div>
-    </div>
-  </body>
-</html>
