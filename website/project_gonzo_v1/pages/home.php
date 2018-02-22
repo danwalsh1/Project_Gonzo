33c8ab1c3626df09b6ffefa964a51fa665bc4395
ElliotHawkins <35804995+ElliotHawkins@users.noreply.github.com>
2018-02-22 11:43:02 +0000

    UPDATE home.php
    Edited text corrections to the home page.

--- website/project_gonzo_v1/pages/home.php
+++ website/project_gonzo_v1/pages/home.php
@@ -25,8 +25,8 @@
       </div>
       
       <div id="Content">
-	  <h1>Project Gonzo</h1>
-		<p>Welcome <?php echo $_SESSION['Username']; ?>, to the Project Gonzo homepage</p>
+		  <h1>Project Gonzo</h1>
+		<p>Welcome <?php echo $_SESSION['Username']; ?>, to the Project Gonzo homepage.</p>
 		 <div id="Content">
 		 <style>
 		 p {
@@ -35,26 +35,21 @@
 		 }	 
 		 </style>
 		<p><img src="../img/gonzoLogo.jpg" alt="Gonzo Logo" align="right" align="bottom" class="res"/>
-			Project Gonzo is an App which you can download on your device, 
-			it records your device's sensor data for example; battery 
-			percentage, CPU usage, brightness, temperature, location and
-			rotation.</p>
-		<p><b>Step 1: Create a free account</b> - To start you off with Project Gonzo 
-		    Our website takes your phone data seriously which is why we are
-			asking every user to create an account with an Username and
-			password. this ensures that only you can see your device data.</p> 
-		<p><b>Step 2: Install the App through our Website</b> - you must then install 
-		    the App on the device you wish to monitor. This can be done by
+			Project Gonzo is an app which you can download on to your device, 
+			it records your device's sensor data, for example your battery 
+			percentage.</p>
+		<p><b>Step 1: Install the app through our Website -</b> you must first install 
+		    the app on the device you wish to monitor. This can be done by
 			accessing the website on your device and clicking the download
 			button on the top left of the screen.</p>
-		<p><b>Step 3: Run the App</b> - Once you have downloaded the App, open it
+		<p><b>Step 2: Run the app -</b> Once you have downloaded the app, open it
 			and click run which is at the center of the screen. This will
-			then start the recording of your device's censor data, the App
-			runs in the background and sends the informations collected to
+			then start the recording of your device's censor data, the app
+			runs in the background and sends the information collected to
 			the websites database where it gets stored.</p>
-		<p><b>Step 4: Now all you do is leave it</b> - censor data will be recorded
+		<p><b>Step 3: Now all you do is leave it -</b> censor data will be recorded
 			every 10 minutes the data recorded can then be viewed anytime 
-			on our website only through your account.</p> 
+			on the <b>My device</b> page displayed as a graph.</p> 
 		<img src="../img/Androidphone.png" align="top" alt="Android Phone" class="res"/>
       </div>
       
