fde52640ceca9bb04b7fbcb62fd08d0b2714845d
Daniel Walsh <33032585+danwalsh1@users.noreply.github.com>
2018-02-22 01:11:48 +0000

    UPDATED home.php
    Moved <nav> tags and <ul> tags into the output of the "displayMainMenu" function.

--- website/project_gonzo_v1/pages/home.php
+++ website/project_gonzo_v1/pages/home.php
@@ -20,11 +20,7 @@
     <div id="Page">
       <div id="Header">
 		<div id="Menu">
-			<nav>
-				<ul>
-					<?php displayMainMenu(); ?>
-				</ul>
-			</nav>
+			<?php displayMainMenu(); ?>
 		</div>
       </div>
       
