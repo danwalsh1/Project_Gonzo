b5f23c5f4b147682fd055b876dd72371e5e5c9d7
Daniel Walsh <33032585+danwalsh1@users.noreply.github.com>
2018-02-22 02:22:02 +0000

    UPDATED generalFunctions.php
    Added code to make the main menu more mobile-friendly.

--- website/project_gonzo_v1/functions/generalFunctions.php
+++ website/project_gonzo_v1/functions/generalFunctions.php
@@ -1,11 +1,13 @@
 <?php
 	function displayMainMenu(){
-		echo '<nav><ul>';
+		echo '<label for="main-menu-control" class="main-menu-control">&#9776;</label>';
+		echo '<input type="checkbox" id="main-menu-control" role="button">';
+		echo '<div id="main-menu"><nav><ul>';
 		if(isset($_SESSION['admin'])){
 			if($_SESSION['admin'] == true){
 				echo '<li><a href="home.php">Home</a></li>
-				<li><a href="adminCharts.php">Sensor Data</a></li
-				><li><a href="AdminPage.php">Administration</a></li>
+				<li><a href="adminCharts.php">Sensor Data</a></li>
+				<li><a href="AdminPage.php">Administration</a></li>
 				<li><a href="createUser.php">Create User</a></li>
 				<li><a href="Downloads.php">Downloads</a></li>
 				<li><a href="logout.php">Logout</a></li>
@@ -20,7 +22,7 @@
 		}else{
 			echo '<li><a href="login.php">Login</a></li><li style="float:right"><a href="home.php"><img src="../img/gonzoLogo.jpg" height="60" width="65" style="align:center" /></a></li>';
 		}
-		echo '</ul></nav>';
+		echo '</ul></nav></div>';
 	}
 	
 	function retrieve_users_DropDown(){
