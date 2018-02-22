15674ca5853ba92da808eac1a0c5a45523144c45
Daniel Walsh <33032585+danwalsh1@users.noreply.github.com>
2018-02-22 01:11:22 +0000

    UPDATED createUser.php
    Moved <nav> tags and <ul> tags into the output of the "displayMainMenu" function.

--- website/project_gonzo_v1/pages/createUser.php
+++ website/project_gonzo_v1/pages/createUser.php
@@ -46,11 +46,7 @@
     <div id="Page">
       <div id="Header">
 		<div id="Menu">
-			<nav>
-				<ul>
-					<?php displayMainMenu(); ?>
-				</ul>
-			</nav>
+			<?php displayMainMenu(); ?>
 		</div>
       </div>
 	    
