4507fa3a9564673dd8269e2f939e123b920ad861
GarethHolloway <33927441+GarethHolloway@users.noreply.github.com>
2018-02-23 15:48:59 +0000

    UPDATED insertData.php
    Added CPU Usage chart information pass through.

--- website/project_gonzo_v1/app_page/insertData.php
+++ website/project_gonzo_v1/app_page/insertData.php
@@ -1,8 +1,9 @@
 <?php
 	include("../functions/sqlFunctions.php");
-	try{
+	
+	if(isset($_GET['batlevel']){	
 		insert_device_data($_GET['deviceid'], $_GET['batlevel'], $_GET['chargestate']);
-	}catch(Exception $e){
-		echo "ERROR: " . $e->getMessage();
+	}elseif(isset($_GET['cpu']){
+		insert_cpu_data($_GET['deviceid'], $_GET['cpu']);
 	}
 ?>
