0d9872ea681ed05ab2f53a1c6d070d18ce6f2407
GarethHolloway <33927441+GarethHolloway@users.noreply.github.com>
2018-02-23 22:01:52 +0000

    UPDATED insertData.php
    Added missing ending parentheses on the if statements.

--- website/project_gonzo_v1/app_page/insertData.php
+++ website/project_gonzo_v1/app_page/insertData.php
@@ -1,9 +1,9 @@
 <?php
 	include("../functions/sqlFunctions.php");
 	
-	if(isset($_GET['batlevel']){	
+	if(isset($_GET['batlevel'])){	
 		insert_device_data($_GET['deviceid'], $_GET['batlevel'], $_GET['chargestate']);
-	}elseif(isset($_GET['cpu']){
+	}elseif(isset($_GET['cpu'])){
 		insert_cpu_data($_GET['deviceid'], $_GET['cpu']);
 	}
 ?>
