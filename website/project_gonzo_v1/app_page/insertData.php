e1f05fe4fcd83297134409cf87dca060edf8c4f8
GarethHolloway <33927441+GarethHolloway@users.noreply.github.com>
2018-02-25 10:56:45 +0000

    UPDATED insertData.php
    Minor change to the function name to be in relation to what each one does.

--- website/project_gonzo_v1/app_page/insertData.php
+++ website/project_gonzo_v1/app_page/insertData.php
@@ -2,8 +2,9 @@
 	include("../functions/sqlFunctions.php");
 	
 	if(isset($_GET['batlevel'])){	
-		insert_device_data($_GET['deviceid'], $_GET['batlevel'], $_GET['chargestate']);
+		insert_battery_data($_GET['deviceid'], $_GET['batlevel'], $_GET['chargestate']);
 	}elseif(isset($_GET['cpu'])){
 		insert_cpu_data($_GET['deviceid'], $_GET['cpu']);
 	}
 ?>
+
