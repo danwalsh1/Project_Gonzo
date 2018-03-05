4f935e5789e126c94f63a770ec147afff886c7f5
Daniel Walsh <33032585+danwalsh1@users.noreply.github.com>
2018-03-05 11:29:50 +0000

    UPDATED USerProfile.php
    Added code to make the page mobile-friendly. This is due to feedback we received from our usability testing.

--- website/project_gonzo_v1/pages/UserProfile.php
+++ website/project_gonzo_v1/pages/UserProfile.php
@@ -36,9 +36,10 @@ function update_user_data(){
 <!DOCTYPE html>
 <html>
   <head>
-  <link href="../CSS/mainMenu.css" rel  = "stylesheet" type="text/CSS" />
-  <link href="../CSS/mainLayout.css" rel  = "stylesheet" type="text/CSS" />
-    <title>User Profile | Project Gonzo</title> <!--Page title -->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--Makes page mobile-friendly-->
+      <link href="../CSS/mainMenu.css" rel  = "stylesheet" type="text/CSS" />
+      <link href="../CSS/mainLayout.css" rel  = "stylesheet" type="text/CSS" />
+      <title>User Profile | Project Gonzo</title> <!--Page title -->
   </head>
   <body>
     <div id="Page">
