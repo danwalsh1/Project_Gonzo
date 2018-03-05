564cd3bab83ba3b864ee2ef8c4e491e1312885b8
GarethHolloway <33927441+GarethHolloway@users.noreply.github.com>
2018-03-05 11:42:34 +0000

    UPDATED generalFunctions.php
    Standardised font to Arial

--- website/project_gonzo_v1/functions/generalFunctions.php
+++ website/project_gonzo_v1/functions/generalFunctions.php
@@ -5,18 +5,18 @@
 		echo '<div id="main-menu"><nav><ul>';
 		if(isset($_SESSION['admin'])){
 			if($_SESSION['admin'] == true){
-				echo '<li><a href="home.php">Home</a></li>
-				<li><a href="adminCharts.php">Charts</a></li>
-				<li><a href="AdminPage.php">Administration</a></li>
-				<li><a href="createUser.php">Create User</a></li>
-				<li><a href="Downloads.php">Downloads</a></li>
-				<li><a href="logout.php">Logout</a></li>
+				echo '<li><a href="home.php"; style="font-family:arial">Home</a></li>
+				<li><a href="adminCharts.php"; style="font-family:arial">Charts</a></li>
+				<li><a href="AdminPage.php"; style="font-family:arial">Administration</a></li>
+				<li><a href="createUser.php"; style="font-family:arial">Create User</a></li>
+				<li><a href="Downloads.php"; style="font-family:arial">Downloads</a></li>
+				<li><a href="logout.php"; style="font-family:arial">Logout</a></li>
 				<li style="float:right"><a href="home.php"><img src="../img/gonzoLogo.jpg" height="60" width="65" style="align:center" /></a></li>';
 			}else{
-				echo '<li><a href="home.php">Home</a></li>
-				<li><a href="userCharts.php">My Device</a></li>
-				<li><a href="UserProfile.php">My Profile</a></li>
-				<li><a href="logout.php">Logout</a></li>
+				echo '<li><a href="home.php"; style="font-family:arial">Home</a></li>
+				<li><a href="userCharts.php"; style="font-family:arial">My Device</a></li>
+				<li><a href="UserProfile.php; style="font-family:arial"">My Profile</a></li>
+				<li><a href="logout.php"; style="font-family:arial">Logout</a></li>
 				<li style="float:right"><a href="home.php"><img src="../img/gonzoLogo.jpg" height="60" width="65" style="align:center" /></a></li>';
 			}
 		}else{
@@ -32,7 +32,6 @@
 	while($row = mysqli_fetch_array($result))
 	{
 		echo $ListofNames="<option>" . $row['username'] . "</option>";
-
 	}
 	}
 ?>
