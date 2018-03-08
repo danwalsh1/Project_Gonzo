297933566b8bc70fda93d3243de93d9342bbf78c
Daniel Walsh <33032585+danwalsh1@users.noreply.github.com>
2018-03-08 19:43:51 +0000

    UPDATED logout.php
    NO CODE CHANGED! Formatted file.

--- website/project_gonzo_v1/pages/logout.php
+++ website/project_gonzo_v1/pages/logout.php
@@ -1,35 +1,35 @@
 <?php
-  session_start();
+	session_start();
   
-  include("../functions/generalFunctions.php");
-  if(isset($_SESSION['Username'])){
-	  unset($_SESSION['Username']);
-  }
-  if(isset($_SESSION['admin'])){
-	  unset($_SESSION['admin']);
-  }
-  session_destroy();
-  Header("Location: login.php");
-  Die();
+	include("../functions/generalFunctions.php");
+	if(isset($_SESSION['Username'])){
+		unset($_SESSION['Username']);
+	}
+	if(isset($_SESSION['admin'])){
+		unset($_SESSION['admin']);
+	}
+	session_destroy();
+	Header("Location: login.php");
+	Die();
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Logout | Project Gonzo</title>
-  </head>
-  <body>
-    <div id="Page">
-      <div id="Header">
-		<div id="Menu">
-			<?php displayMainMenu(); ?>
+	<head>
+		<title>Logout | Project Gonzo</title>
+	</head>
+	<body>
+		<div id="Page">
+			<div id="Header">
+				<div id="Menu">
+					<?php displayMainMenu(); ?>
+				</div>
+			</div>
+      
+			<div id="Content">
+			</div>
+      
+			<div id="Footer">
+			</div>
 		</div>
-      </div>
-      
-      <div id="Content">
-      </div>
-      
-      <div id="Footer">
-      </div>
-    </div>
-  </body>
+	</body>
 </html>
