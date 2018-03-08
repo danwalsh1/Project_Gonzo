8d875f0c518bbd1ce5e3b4c04d17d49bfd68d3d5
Daniel Walsh <33032585+danwalsh1@users.noreply.github.com>
2018-03-08 14:36:53 +0000

    UPDATED createUser.php
    Formatted code and put form into a table.

--- website/project_gonzo_v1/pages/createUser.php
+++ website/project_gonzo_v1/pages/createUser.php
@@ -1,116 +1,152 @@
 <?php
-  session_start(); #Starts a session.
+	session_start(); #Starts a session.
   
-  include("../functions/generalFunctions.php"); #Import additional functionality stored in seperate files.
-  include("../functions/sqlFunctions.php");
+	include("../functions/generalFunctions.php"); #Import additional functionality stored in seperate files.
+	include("../functions/sqlFunctions.php");
 
-  if(isset($_SESSION['Username']) == false){
-	  header("Location: login.php");
-	  die();
-  }
+	if(isset($_SESSION['Username']) == false){
+		header("Location: login.php");
+		die();
+	}
 
-  if(isset($_SESSION['admin'])){
-	  if($_SESSION['admin'] == False){
-		  header("Location: home.php");
-		  die();
-	  }
-  }else{
-	  header("Location: logout.php");
-	  die();
-  }
+	if(isset($_SESSION['admin'])){
+		if($_SESSION['admin'] == False){
+			header("Location: home.php");
+			die();
+		}
+	}else{
+		header("Location: logout.php");
+		die();
+	}
   
-  if(isset($_POST['createUser'])){
-	  if(ctype_alnum($_POST['username']) and ctype_alnum($_POST['forename']) and ctype_alnum($_POST['surname']) and ctype_alnum($_POST['deviceid']) and ctype_alnum($_POST['phonenumber']) and ctype_alnum($_POST['password']) and filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
-		if(isset($_POST['admin'])){
-			if($_POST['admin'] == true){
-				$admin = 1;
+	if(isset($_POST['createUser'])){
+		if(ctype_alnum($_POST['username']) and ctype_alnum($_POST['forename']) and ctype_alnum($_POST['surname']) and ctype_alnum($_POST['deviceid']) and ctype_alnum($_POST['phonenumber']) and ctype_alnum($_POST['password']) and filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+			if(isset($_POST['admin'])){
+				if($_POST['admin'] == true){
+					$admin = 1;
+				}else{
+					$admin = 0;
+				}
 			}else{
 				$admin = 0;
 			}
-		}else{
-			$admin = 0;
-		}
-	  $connect = connect_db();   # Connects to data base
-	  $sql = "INSERT INTO users(username, password, admin, forename, surname, device_id, phone_num, email) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";   # Queries the data base for user details 
-	  $stmt = $connect->prepare($sql);
-	  $stmt->bind_param('ssisssss', $_POST['username'], $_POST['password'], $admin, $_POST['forename'], $_POST['surname'], $_POST['deviceid'], $_POST['phonenumber'], $_POST['email']);
-	  $stmt->execute();
+			$connect = connect_db();   # Connects to data base
+			$sql = "INSERT INTO users(username, password, admin, forename, surname, device_id, phone_num, email) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";   # Queries the data base for user details 
+			$stmt = $connect->prepare($sql);
+			$stmt->bind_param('ssisssss', $_POST['username'], $_POST['password'], $admin, $_POST['forename'], $_POST['surname'], $_POST['deviceid'], $_POST['phonenumber'], $_POST['email']);
+			$stmt->execute();
 	  
-	  $msg = "New user created with the username: " . $_POST['username'];
-	  messages($msg, "createUser.php", 10);
-		
-	  }
-  }
-
+			$msg = "New user created with the username: " . $_POST['username'];
+			messages($msg, "createUser.php", 10);
+		}
+	}
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--Makes page mobile-friendly-->
-    <link href="../CSS/mainMenu.css" rel  = "stylesheet" type="text/CSS" />
-    <link href="../CSS/mainLayout.css" rel  = "stylesheet" type="text/CSS" />
-    <title>Create User | Project Gonzo</title> <!--Page title -->
-  </head>
-  <body>
-    <div id="Page">
-      <div id="Header">
-		<div id="Menu">
-			<?php displayMainMenu(); ?>
-		</div>
-      </div>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--Makes page mobile-friendly-->
+		<link href="../CSS/mainMenu.css" rel  = "stylesheet" type="text/CSS" />
+		<link href="../CSS/mainLayout.css" rel  = "stylesheet" type="text/CSS" />
+		<title>Create User | Project Gonzo</title> <!--Page title -->
+	</head>
+	<body>
+		<div id="Page">
+			<div id="Header">
+				<div id="Menu">
+					<?php displayMainMenu(); ?>
+				</div>
+			</div>
 	    
-      <div id="Content">
-		<div id="createUserForm" style="text-align: center;font-family:arial">
-			<form action="createUser.php" method="POST">
-				<div id="Username" style="padding-top: 10px;">
-					<label>Username:</label>
-					<input type ="text" name="username">   
-				</div>
+			<div id="Content">
+				<div id="createUserForm" style="font-family:arial">
+					<table align="center">
+						<form action="createUser.php" method="POST">
+							<tr>
+								<td>
+									<label>Username:</label>
+								</td>
+								<td>
+									<input type ="text" name="username">   
+								</td>
+							</tr>
 			
-				<div id="Forename" style="padding-top: 10px;font-family:arial">
-					<label>Forename:</label>
-					<input type ="text" name="forename">
-				</div>
+							<tr>
+								<td>
+									<label>Forename:</label>
+								</td>
+								<td>
+									<input type ="text" name="forename">
+								</td>
+							</tr>
 		
-				<div id="Surname" style="padding-top: 10px;font-family:arial">
-					<label>Surname:</label>
-					<input type="text" name ="surname">
-				</div>
+							<tr>
+								<td>
+									<label>Surname:</label>
+								</td>
+								<td>
+									<input type="text" name ="surname">
+								</td>
+							</tr>
 		
-				<div id= "Device_ID" style="padding-top: 10px;font-family:arial">
-					<label>Device ID:</label>
-					<input type ="text" name="deviceid">
-				</div>
+							<tr>
+								<td>
+									<label>Device ID:</label>
+								</td>
+								<td>
+									<input type ="text" name="deviceid">
+								</td>
+							</tr>
 		 
-				<div id= "Phone_Number" style="padding-top: 10px;font-family:arial">
-					<label>Phone Number:</label>
-					<input type ="text" name="phonenumber">
-				</div>
+							<tr>
+								<td>
+									<label>Phone Number:</label>
+								</td>
+								<td>
+									<input type ="text" name="phonenumber">
+								</td>
+							</tr>
 		
-				<div id= "Email_Address" style="padding-top: 10px;font-family:arial">
-					<label>Email Address:</label>
-					<input type ="text" name="email">
-				</div>
+							<tr>
+								<td>
+									<label>Email Address:</label>
+								</td>
+								<td>
+									<input type ="text" name="email">
+								</td>
+							</tr>
 		
-				<div id= "Password" style="padding-top: 10px;font-family:arial">
-					<label>Password:</label>
-					<input type ="text" name="password">
-				</div>
+							<tr>
+								<td>
+									<label>Password:</label>
+								</td>
+								<td>
+									<input type ="text" name="password">
+								</td>
+							</tr>
 				
-				<div id="Admin" style="padding-top: 10px;font-family:arial">
-					<label>Admin:</label>
-					<input type="checkbox" name="admin">
-				</div>
+							<tr>
+								<td>
+									<label>Admin:</label>
+								</td>
+								<td>
+									<input type="checkbox" name="admin">
+								</td>
+							</tr>
 			
-				<div id= "Submit" style="padding-top: 10px;font-family:arial">
-					<input type="submit" value="Create User" name="createUser">
+							<tr>
+								<td>
+								</td>
+								<td>
+									<input type="submit" value="Create User" name="createUser">
+								</td>
+							</tr>
+						</form>
+					</table>
 				</div>
-			</form>
-		</div>
-	  </div>
+			</div>
 	  
-      <div id="Footer">
-      </div>
-    </div>
-  </body>
+			<div id="Footer">
+			</div>
+		</div>
+	</body>
 </html>
