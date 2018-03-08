353daf9ab0cae0ed84b0d94ea858a8f418a61706
Daniel Walsh <33032585+danwalsh1@users.noreply.github.com>
2018-03-08 12:39:35 +0000

    UPDATED AdminPage.php
    NO CODE CHANGED. Format change only.

--- website/project_gonzo_v1/pages/AdminPage.php
+++ website/project_gonzo_v1/pages/AdminPage.php
@@ -1,40 +1,41 @@
 <?php
-  session_start(); #Starts a session.
+    session_start(); #Starts a session.
   
-  include("../functions/generalFunctions.php"); #Import additional functionality stored in seperate files.
-  include("../functions/sqlFunctions.php");
+	include("../functions/generalFunctions.php"); #Import additional functionality stored in seperate files.
+	include("../functions/sqlFunctions.php");
 
-  if(!isset($_SESSION['Username'])){ #Checks the session username is set from the login page and then kills off the login page after.
-	  header("Location: login.php");
-	  die();
-  }
-if(isset($_SESSION['admin'])){
-	  if($_SESSION['admin'] == False){
-		  header("Location: home.php");
-		  die();
-	  }
-  }else{
-	  header("Location: logout.php");
-	  die();
-  }
-  
-  
-check_db_exists();  #Checks database existence by calling the function stored in sqlFunctions.php
+	if(!isset($_SESSION['Username'])){ #Checks the session username is set from the login page and then kills off the login page after.
+		header("Location: login.php");
+		die();
+	}
 	
-if (isset($_POST['SubmitProfileForm'])){
+	if(isset($_SESSION['admin'])){
+		if($_SESSION['admin'] == False){
+			header("Location: home.php");
+			die();
+		}
+	}else{
+		header("Location: logout.php");
+		die();
+	}
+  
+  
+	check_db_exists();  #Checks database existence by calling the function stored in sqlFunctions.php
+	
+	if(isset($_POST['SubmitProfileForm'])){
 		#If the data entry type is correct and equates to true, calls the update user data function.
-	$result = update_user_data();
+		$result = update_user_data();
 	}
 	
 
-if (isset($_POST['UpdateDetailsView'])){
-	$UserSelected = $_POST['DD'];
+	if(isset($_POST['UpdateDetailsView'])){
+		$UserSelected = $_POST['DD'];
 		#Checked for validity incase of sql injection attempts.
-	$result = fetch_user_data($UserSelected); #Runs the fetch function and displays in the form fields.
+		$result = fetch_user_data($UserSelected); #Runs the fetch function and displays in the form fields.
 	}
 		
 
-function fetch_user_data($UserSelected){
+	function fetch_user_data($UserSelected){
 		$connect = connect_db("NULL");
 		$sql = 'SELECT * FROM users WHERE username = ?';
 		$stmt = $connect->prepare($sql);
@@ -44,24 +45,23 @@ function fetch_user_data($UserSelected){
 		$row = $result->fetch_assoc();
 			
 		return array($row['username'], $row['forename'], $row['surname'], $row['device_id'], $row['phone_num'], $row['email'],$row['password']);		
-		}
-			
+	}		
 		
-function update_user_data(){
-       		$connect = connect_db("NULL"); #Connects to DB
+	function update_user_data(){
+       	$connect = connect_db("NULL"); #Connects to DB
 		$stmt = "UPDATE users SET forename = ?, surname = ?, device_id = ? , phone_num = ?, email = ?, password = ? WHERE username = ?";
-        	$sql = $connect->prepare($stmt);
+        $sql = $connect->prepare($stmt);
 		$sql->bind_param('sssssss', $_POST['forename'], $_POST['surname'],$_POST['deviceid'], $_POST['phonenum'], $_POST['email'], $_POST['PasswordUpdate'], $_POST['username']);
 		$sql->execute();
 		$sql->close();
 		
 		$msg = "Users information has now been updated!";
 		messages($msg, "AdminPage.php", 10);
-		}
+	}
 
-if(isset($UserSelected)){
-	$result = fetch_user_data($UserSelected);
-}
+	if(isset($UserSelected)){
+		$result = fetch_user_data($UserSelected);
+	}
 ?>
 <!DOCTYPE html>
 <html>
