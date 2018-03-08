bf9dea846fc03222965775705148152536bbec95
ElliotHawkins <35804995+ElliotHawkins@users.noreply.github.com>
2018-03-08 13:19:21 +0000

    FORMATED Downloads.php
    No Code Changed, only formated

--- website/project_gonzo_v1/pages/Downloads.php
+++ website/project_gonzo_v1/pages/Downloads.php
@@ -1,57 +1,60 @@
 <?php
 
-  session_start(); #Starts a session.
+	session_start(); #Starts a session.
 
-  include("../functions/generalFunctions.php");
+	include("../functions/generalFunctions.php");
 
-  if(isset($_SESSION['Username']) == false){
+	if(isset($_SESSION['Username']) == false){
 	  header("Location: login.php");
 	  die();
-  }
-  if(isset($_SESSION['admin'])){
-	  if($_SESSION['admin'] == False){
-		  header("Location: home.php");
-		  die();
-	  }
-  }else{
-	  header("Location: logout.php");
-	  die();
-  }
+	}
+	
+	if(isset($_SESSION['admin'])){
+		if($_SESSION['admin'] == False){
+			header("Location: home.php");
+			die();
+		}
+	}
+	
+	else{
+		header("Location: logout.php");
+		die();
+		}
 ?>
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--Makes page mobile-friendly-->
-    <link href="../CSS/mainMenu.css" rel  = "stylesheet" type="text/CSS" />
-    <link href="../CSS/mainLayout.css" rel  = "stylesheet" type="text/CSS" />
-    <title>Downloads | Project Gonzo</title> <!--Project title -->
-  </head>
-  <body>
-    <div id="Page">
-      <div id="Header">
-		<div id="Menu">
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--Makes page mobile-friendly-->
+		<link href="../CSS/mainMenu.css" rel  = "stylesheet" type="text/CSS" />
+		<link href="../CSS/mainLayout.css" rel  = "stylesheet" type="text/CSS" />
+		<title>Downloads | Project Gonzo</title> <!--Project title -->
+	</head>
+	<body>
+		<div id="Page">
+			<div id="Header">
+				<div id="Menu">
 			<?php displayMainMenu(); ?>
+				</div>
+			</div>
+		<div style = "padding-left: 10px;font-family:arial">
+			<p><u>Installation Instructions - Windows Desktop Application</u></p>
+			<p>1. If you haven't already, click on the app link.</p>
+			<p>2. Unzip the download contents.</p>
+			<p>3. Navigate into the folder and run the 'setup.exe' file. </p>
+			<p>4. When prompted press install, the app will automatically open when installation is complete.</p>
+			<p>5. Congratulations, you can now start logging data :) </p>
+
+			<div style= padding-top:10px; >
+				<form action=https://projectgonzocoventry.000webhostapp.com/downloads/MSAPP.zip>
+					<input type="submit" value="Click here for the desktop app." />
+				</form>
+			<br><br>
+			<form action=https://projectgonzocoventry.000webhostapp.com/downloads/MobileApp.zip>
+				<input type="submit" value="Click here for the mobile app." />
+			</form>
+
+			</div>
 		</div>
-      </div>
-
-	<div style = "padding-left: 10px;font-family:arial">
-	<p><u>Installation Instructions - Windows Desktop Application</u></p>
-	<p>1. If you haven't already, click on the app link.</p>
-	<p>2. Unzip the download contents.</p>
-	<p>3. Navigate into the folder and run the 'setup.exe' file. </p>
-	<p>4. When prompted press install, the app will automatically open when installation is complete.</p>
-	<p>5. Congratulations, you can now start logging data :) </p>
-
-	<div style= padding-top:10px; >
-    <form action=https://projectgonzocoventry.000webhostapp.com/downloads/MSAPP.zip>
-    <input type="submit" value="Click here for the desktop app." />
-    </form>
-    <br><br>
-    <form action=https://projectgonzocoventry.000webhostapp.com/downloads/MobileApp.zip>
-    <input type="submit" value="Click here for the mobile app." />
-    </form>
-
-	</div>
 	</body>
 </html>
