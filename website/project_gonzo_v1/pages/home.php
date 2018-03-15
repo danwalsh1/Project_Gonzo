c9fd88f7585a2d27266b29d4bebc014c338a8cf9
Jacob Stenson <jacob@nosnets.co.uk>
2018-03-15 18:02:16 +0000

    UPDATED Home.php
    Commented.

--- website/project_gonzo_v1/pages/home.php
+++ website/project_gonzo_v1/pages/home.php
@@ -1,8 +1,8 @@
 <?php
 	session_start();
-	include("../functions/generalFunctions.php");
-  
-	if(!isset($_SESSION['Username'])){
+	include("../functions/generalFunctions.php"); #Allows functionality from the 'generalFunctions.php' page.
+
+	if(!isset($_SESSION['Username'])){            #If the current session does not have a username set to it then the user will be taken to the login.php page.
 		header("Location: login.php");
 		die();
 	}
@@ -10,32 +10,32 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--Makes page mobile-friendly-->
-		<link href="../CSS/mainMenu.css" rel  = "stylesheet" type="text/CSS" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">        <!--Makes page mobile-friendly-->
+		<link href="../CSS/mainMenu.css" rel  = "stylesheet" type="text/CSS" />       <!--Gets the css for this page from the 'mainMenu.css' and 'mainLayout.css' files-->
 		<link href="../CSS/mainLayout.css" rel  = "stylesheet" type="text/CSS" />
-		<title>Home | Project Gonzo</title>
+		<title>Home | Project Gonzo</title>                                           <!--Project title -->
 	</head>
 	<body>
 		<div id="Page">
 			<div id="Header">
 				<div id="Menu">
-					<?php displayMainMenu(); ?>
+					<?php displayMainMenu(); ?>                                             <!--Calls the displayMainMenu function to show the main menu at the top of the page.-->
 				</div>
 			</div>
-			<div id="Content">
+			<div id="Content">																													<!--Opens a new div for Header1 and the welcome message.-->
 				<h1 style="font-family:arial">Project Gonzo</h1>
-					<p style="font-family:arial">Welcome <?php echo $_SESSION['Username']; ?>, to the Project Gonzo homepage.</p>
-				<div id="Content">
-					<p style="font-family:arial"><img src="../img/gonzoLogo.jpg" alt="Gonzo Logo" align="right" align="bottom" class="res"/>
+					<p style="font-family:arial">Welcome <?php echo $_SESSION['Username']; ?>, to the Project Gonzo homepage.</p>			 					<!--The php echo code here allows the text to be personalised to the user, so the site welcomes them by their username-->
+				<div id="Content">																																																	 					<!--Opens the Content div again, seperating the Header1 and welcome message from the text below.-->
+					<p style="font-family:arial"><img src="../img/gonzoLogo.jpg" alt="Gonzo Logo" align="right" align="bottom" class="res"/>		<!--Allows the image from the given folder to be used on the website, on the right at the bottom of the div.-->
 					Project Gonzo is a data collection tool that retrieves information for the
 					battery level, charging state and CPU usage from a Windows machine and the battery level and charging state from an Android device.</p>
-					<p style="font-family:arial">If the application hasn't been installed already, your administrator can install the application on 
+					<p style="font-family:arial">If the application hasn't been installed already, your administrator can install the application on
 					your device by navigating to the downloads page.</p>
-					<p style="font-family:arial">To access historical data and produce charts please select either 'My Device' or 'Charts', 
+					<p style="font-family:arial">To access historical data and produce charts please select either 'My Device' or 'Charts',
 					dependant on your permission level.</p>
-					<img src="../img/AndroidPhoneUpdated.png" align="top" alt="Android Phone" class="res"/>
+					<img src="../img/AndroidPhoneUpdated.png" align="top" alt="Android Phone" class="res"/> 																		<!-- Allows the image from the given folder to be used on the website as high as possible after the text.-->
 				</div>
-      
+
 				<div id="Footer">
 				</div>
 			</div>
