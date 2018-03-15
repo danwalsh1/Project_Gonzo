3f4dd88141ed45f4b67455c11c4e14d0ea59e194
Jacob Stenson <jacob@nosnets.co.uk>
2018-03-15 12:56:35 +0000

    UPDATED insertData.php
    Commented.

--- website/project_gonzo_v1/app_page/insertData.php
+++ website/project_gonzo_v1/app_page/insertData.php
@@ -1,10 +1,10 @@
 <?php
-	include("../functions/sqlFunctions.php");
+	include("../functions/sqlFunctions.php"); #Allows functinality from the 'sqlFunctions.php' page.
 	
-	if(isset($_GET['batlevel'])){	
-		insert_battery_data($_GET['deviceid'], $_GET['batlevel'], $_GET['chargestate']);
-	}elseif(isset($_GET['cpu'])){
-		insert_cpu_data($_GET['deviceid'], $_GET['cpu']);
+	if(isset($_GET['batlevel'])){	#If 'batlevel' is inside the URL then the if statement is accessed.
+		insert_battery_data($_GET['deviceid'], $_GET['batlevel'], $_GET['chargestate']); #Runs the 'insert_battery_data_' function and passes in the variables 'deviceid', 'batlevel' and 'chargestate'.
+	}elseif(isset($_GET['cpu'])){	#If 'cpu' is inside the URL then the if statetment is accessed.
+		insert_cpu_data($_GET['deviceid'], $_GET['cpu']); #Runs the 'insert_cpu_data' and passes in then variables 'deviceid' and 'cpu'.
 	}
 ?>
 
